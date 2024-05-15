ae550bcb2453bd6e576680e475d9c341bbab77ce
José Ramón <167759297+JRamonPY@users.noreply.github.com>
2024-05-15 19:24:07 +0200

    Delete Join en SQL directory

--- Join en SQL/03rightjoin.sql
+++
@@ -1,7 +0,0 @@
-Select name, dni_number from users
-right join dni
-on users.user_id = dni.user_id
-
---lo mismo que el left pero al revés, nos quedamos con lo común y l
---de la tabla de la derecha
-
--- Join en SQL/04fulljoin.sql
+++
@@ -1,6 +0,0 @@
-Select name, dni_number from users
-left join dni
-on users.user_id = dni.user_id
-
---lo cogeremos todo, tanto lo comun como lo que no
---hay que tener cuidadoº
